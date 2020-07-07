2a1e51c6787df1fdf2b2447f85d0f4f1cabb6697
Jasmine <jasspears@gmail.com>
2020-07-06 21:00:35 -0500

    django initialized

---
+++ ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/JKSpears/Documents/School/pythonCC/ch1820/learning_log/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
